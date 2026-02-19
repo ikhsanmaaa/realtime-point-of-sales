a6be8e13224e5c28bebcbbbba0e71bcb1319b340
ikhsanmaaa <ikhsanmaaa@gmail.com>
2026-02-19 16:11:22 +0700

    add dashboard menu layout

---
+++ src/app/(dashboard)/admin/user/page.tsx
@@ -0,0 +1,9 @@
+import UserManagement from "./_components/user";
+
+export const metadata = {
+  title: "Cafe APP | User Management",
+};
+
+export default function UserManagementPage() {
+  return <UserManagement />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import { DarkModeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Input />
-      <Button>Hello</Button>
-      <DarkModeToggle />
+    <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+      <h1 className="text-4xl font-semibold ">WELCOME KING AVIP</h1>
+      <Link href="/admin">
+        <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }

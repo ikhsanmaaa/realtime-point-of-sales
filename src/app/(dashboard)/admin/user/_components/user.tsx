d9398151dd314a5d3fa9d1446d6275440440b83a
ikhsanmaaa <ikhsanmaaa@gmail.com>
2026-02-20 15:29:59 +0700

    add data table user

--- src/app/(dashboard)/admin/user/_components/user.tsx
+++ src/app/(dashboard)/admin/user/_components/user.tsx
@@ -1,35 +1,98 @@
 "use client";
 
+import DataTable from "@/components/common/data-table";
+import DropdownAction from "@/components/common/dropdown-action";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { HEADER_TABLE_USER } from "@/constants/user-constant";
+import useDataTable from "@/hooks/use-data-table";
 import { createClient } from "@/lib/supabase/client";
 import { useQuery } from "@tanstack/react-query";
+import { Pencil, Trash2 } from "lucide-react";
+import { useMemo } from "react";
 import { toast } from "sonner";
 
 export default function UserManagement() {
   const supabase = createClient();
+
+  const {
+    currentPage,
+    currentLimit,
+    currentSearch,
+    handleChangeLimit,
+    handleChangePage,
+    handleChangeSearch,
+  } = useDataTable();
+
   const { data: users, isLoading } = useQuery({
-    queryKey: ["users"],
+    queryKey: ["users", currentPage, currentLimit, currentSearch],
     queryFn: async () => {
-      const { data, error } = await supabase
+      const result = await supabase
         .from("profiles")
         .select("*", { count: "exact" })
-        .order("created_at");
+        .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
+        .order("created_at")
+        .ilike("name", `%${currentSearch}%`);
 
-      if (error)
-        toast.error("Get User data failed", { description: error.message });
+      if (result.error)
+        toast.error("Get User data failed", {
+          description: result.error.message,
+        });
 
-      return data;
+      return result;
     },
   });
+
+  const filterData = useMemo(() => {
+    return (users?.data || []).map((user, index) => {
+      return [
+        index + 1,
+        user.id,
+        user.name,
+        user.role,
+        <DropdownAction
+          menu={[
+            {
+              label: (
+                <span className="flex item-center gap-2">
+                  <Pencil />
+                  Edit
+                </span>
+              ),
+              action: () => {},
+            },
+            {
+              label: (
+                <span className="flex item-center gap-2">
+                  <Trash2 className="text-red-400" />
+                  Delete
+                </span>
+              ),
+              variant: "destructive",
+              action: () => {},
+            },
+          ]}
+        />,
+      ];
+    });
+  }, [users]);
+
+  const totalPages = useMemo(() => {
+    return users && users.count !== null
+      ? Math.ceil(users.count / currentLimit)
+      : 0;
+  }, [users]);
 
   return (
     <div className="w-full">
       <div className="flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full">
         <h1 className="text-2xl font-bold">User Management</h1>
         <div className="flex gap-2">
-          <Input placeholder="Search by name" />
+          <Input
+            placeholder="Search by name"
+            onChange={(e) => handleChangeSearch(e.target.value)}
+          />
           <Dialog>
             <DialogTrigger asChild>
               <Button variant="outline">Create</Button>
@@ -37,12 +100,16 @@ export default function UserManagement() {
           </Dialog>
         </div>
       </div>
-      {users?.map((user) => (
-        <div key={user.id}>
-          <h2>{user.name}</h2>
-          <h2>{user.role}</h2>
-        </div>
-      ))}
+      <DataTable
+        header={HEADER_TABLE_USER}
+        isLoading={isLoading}
+        data={filterData}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        currentLimit={currentLimit}
+        onChangePage={handleChangePage}
+        onChangeLimit={handleChangeLimit}
+      />
     </div>
   );
 }
---
+++ src/hooks/use-data-table.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { DEFAULT_LIMIT, DEFAULT_PAGE } from "@/constants/data-table-constant";
+import { Search } from "lucide-react";
+import { useState } from "react";
+import useDebounce from "./use-debounce";
+
+export default function useDataTable() {
+  const debounce = useDebounce();
+
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
+  const [currentLimit, setCurrentLimit] = useState(DEFAULT_LIMIT);
+  const [currentSearch, setCurrentSearch] = useState("");
+  const handleChangePage = (page: number) => {
+    setCurrentPage(page);
+  };
+  const handleChangeLimit = (limit: number) => {
+    setCurrentLimit(limit);
+    setCurrentPage(DEFAULT_PAGE);
+  };
+
+  const handleChangeSearch = (search: string) => {
+    debounce(() => {
+      setCurrentSearch(search);
+      setCurrentPage(DEFAULT_PAGE);
+    }, 500);
+  };
+
+  return {
+    currentPage,
+    currentLimit,
+    currentSearch,
+
+    handleChangeLimit,
+    handleChangePage,
+    handleChangeSearch,
+  };
+}
